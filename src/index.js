c7a461578e02390967cc604d811ca069562ed75d
Kevin Zhou <kevinzhou2222@gmail.com>
2020-06-06 20:39:34 -0400

    Add Material-UI and favicon

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'fontsource-roboto/latin-ext-300.css';
+import 'fontsource-roboto/latin-ext-400.css';
+import 'fontsource-roboto/latin-ext-500.css';
+import 'fontsource-roboto/latin-ext-700.css';
+
+import Typography from '@material-ui/core/Typography';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Typography variant={'h2'}>Polar Roses</Typography>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
